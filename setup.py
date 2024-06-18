c5420fcded79500e134ae70ff2fde03756ab5052
jprice <jprice@izeni.com>
2024-06-18 00:21:32 -0600

    Bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="punkweb_insight",
-    version="0.0.2",
+    version="0.0.3",
     author="Punkweb",
     author_email="punkwebnet@gmail.com",
     packages=["punkweb_insight"],
